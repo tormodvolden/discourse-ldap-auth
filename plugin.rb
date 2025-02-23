b081790639a78d2c2a661b87c3b28383d6b1a33e
Tormod Volden <debian.tormod@gmail.com>
2025-02-23 14:14:07 +0100

    Bump omniauth-ldap to 2.0.0
    Signed-off-by: Tormod Volden <debian.tormod@gmail.com>

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,7 @@ enabled_site_setting :ldap_enabled
 gem 'pyu-ruby-sasl', '0.0.3.3', require: false
 gem 'rubyntlm', '0.3.4', require: false
 gem 'net-ldap', '0.17.1'
-gem 'omniauth-ldap', '1.0.5'
+gem 'omniauth-ldap', '2.0.0'
 
 require 'yaml'
 require_relative 'lib/ldap_user'
